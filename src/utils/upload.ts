6698f35badc8bf57f1bb305310ea732190e8a1c3
hoooodboy <67892230+hoooodboy@users.noreply.github.com>
2025-02-27 18:53:57 +0900

    feat: img host

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,5 +1,5 @@
 const imgView = (url: string) => {
-  return `https://bk-vahana.s3.ap-northeast-2.amazonaws.com/${url}?w=420&f=cover` as string;
+  return `https://bk-vahana.s3.ap-northeast-2.amazonaws.com${url}?w=420&f=cover` as string;
 };
 
 export { imgView };
