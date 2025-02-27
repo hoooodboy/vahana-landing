d632ca0ba2353111982d723d7b3298dfc410e22f
hoooodboy <67892230+hoooodboy@users.noreply.github.com>
2025-02-27 18:51:19 +0900

    feat: img

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,5 +1,5 @@
 const imgView = (url: string) => {
-  return `https://d37qux5i5ucae.cloudfront.net${url}?w=420&f=cover` as string;
+  return `https://bk-vahana.s3.ap-northeast-2.amazonaws.com/${url}?w=420&f=cover` as string;
 };
 
 export { imgView };
